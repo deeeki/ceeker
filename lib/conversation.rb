0582b6ed6a31024a324d9d55d7861ef7224c385d
deeeki <deeeki@gmail.com>
2013-05-06 19:36:49 +0900

    Define scopes for hourly mail

--- lib/conversation.rb
+++ lib/conversation.rb
@@ -2,6 +2,10 @@ class Conversation
   include Mongoid::Document
   embeds_many :tweets, class_name: 'Conversation::Tweet'
 
+  scope :lang, ->(lang = 'ja'){ where(lang: lang) }
+  scope :only_1hour, ->(to = Time.now){ where(:created_at.gte => to - 1.hour, :created_at.lt => to) }
+  scope :by_priority, ->{ order_by(average_length: :desc) }
+
   class << self
     def create_with_tweets *tweets
       root_tweet_id = tweets.first.in_reply_to_status_id || tweets.first.id

38872d98229f13fdc233964c26a968b436a87b07
deeeki <deeeki@gmail.com>
2013-05-16 22:58:49 +0900

    Implement reduction process

---
+++ reduction.rb
@@ -0,0 +1,17 @@
+require 'bundler/setup'
+Bundler.require(:default, :process)
+
+Dotenv.load
+
+Mongoid.load!('config/mongoid.yml', ENV['RACK_ENV'] || :development)
+
+require 'active_support/dependencies'
+ActiveSupport::Dependencies.autoload_paths << File.expand_path('../lib', __FILE__)
+
+stats = Mongoid.default_session.command(dbStats: 1)
+exit if stats['fileSize'] < 500000000 # MongoLab limit is 520093696
+
+Tweet.lt(id: Tweet.skip(15000).first.id).delete_all
+Conversation.lt(id: Conversation.skip(1500).first.id).delete_all
+
+Mongoid.default_session.command(repairDatabase: 1)
--- worker.sh
+++ worker.sh
@@ -2,4 +2,5 @@ WORKER_FILE=collection.rb
 if [ `pgrep -f $WORKER_FILE | wc -l` = 1 ]; then
 	pkill -f $WORKER_FILE
 fi
+ruby reduction.rb
 ruby $WORKER_FILE

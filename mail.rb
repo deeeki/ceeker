592fcf47dda3efc1a93340da57833f87de4beb5b
deeeki <deeeki@gmail.com>
2014-05-18 02:54:29 +0900

    Use letter_opener on development

--- mail.rb
+++ mail.rb
@@ -1,5 +1,5 @@
 require 'bundler/setup'
-Bundler.require(:default, :mail)
+Bundler.require(:default, :mail, ENV['RACK_ENV'] || :development)
 
 Dotenv.load
 
@@ -11,14 +11,19 @@ ActiveSupport::Dependencies.autoload_paths << File.expand_path('../lib', __FILE_
 Sprockets.append_path(File.expand_path('../assets/stylesheets', __FILE__))
 
 ActionMailer::Base.append_view_path(File.expand_path('../views', __FILE__))
-ActionMailer::Base.smtp_settings = {
-  :address        => ENV['MAILGUN_SMTP_SERVER'],
-  :port           => ENV['MAILGUN_SMTP_PORT'],
-  :domain         => 'heroku.com',
-  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-  :authentication => :plain,
-}
+if defined?(LetterOpener)
+  ActionMailer::Base.add_delivery_method :letter_opener, LetterOpener::DeliveryMethod, location: File.expand_path('../tmp/letter_opener', __FILE__)
+  ActionMailer::Base.delivery_method = :letter_opener
+else
+  ActionMailer::Base.smtp_settings = {
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :domain         => 'heroku.com',
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :authentication => :plain,
+  }
+end
 
 require 'action_dispatch/http/mime_type'
 ActionView::Template::Types.delegate_to(Mime) # neccesary to detect appropriate Content-Type

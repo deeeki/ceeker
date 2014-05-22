866b836cfde26d0baa9515fbfcf283acf062f287
deeeki <deeeki@gmail.com>
2014-05-22 21:39:21 +0900

    Enable mailer to specify language

--- lib/app_mailer.rb
+++ lib/app_mailer.rb
@@ -6,17 +6,17 @@ class AppMailer < ActionMailer::Base
 
   def url_options; end
 
-  def hour time_to
+  def hour time_to, lang: :ja
     @time_from = time_to - 1.hour
-    @conversations = Conversation.during_hour_to(time_to).lang(:ja).by_priority
+    @conversations = Conversation.during_hour_to(time_to).lang(lang).by_priority
     mail(
       subject: "Conversations at #{@time_from.getlocal.strftime('%H on %b %d')}",
     )
   end
 
-  def day date
+  def day date, lang: :ja
     @date = date
-    @conversations = Conversation.during_day_on(@date).lang(:ja).by_priority.limit(100)
+    @conversations = Conversation.during_day_on(@date).lang(lang).by_priority.limit(100)
     mail(
       subject: "Top Conversations on #{@date.strftime('%b %d')}",
     )

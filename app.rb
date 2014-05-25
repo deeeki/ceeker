1438152cff9f2227b11c7d86752a3f6681e9b5f8
deeeki <deeeki@gmail.com>
2014-05-25 13:19:40 +0900

    Implement autopager

--- app.rb
+++ app.rb
@@ -29,12 +29,12 @@ class App < Sinatra::Base
 
   get '/' do
     @conversations = Conversation.order(ended_at: :desc).lang(@lang).page(current_page, 50)
-    slim :index
+    slim :index, layout: !request.xhr?
   end
 
   get %r[\A/(\d{4})/(\d{2})/(\d{2})\z] do |year, month, day|
     @date = Date.new(year.to_i, month.to_i, day.to_i)
     @conversations = Conversation.during_day_on(@date).lang(@lang).by_priority.page(current_page, 50)
-    slim :index
+    slim :index, layout: !request.xhr?
   end
 end
--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -1,3 +1,25 @@
 //= require jquery/dist/jquery.js
 //= require bootstrap-sass/js/dropdown.js
 //= require_tree .
+
+$(function(){
+  var content = $('.conversations');
+  var nextUrl = $('a[rel=next]').attr('href')
+  var loading = false;
+
+  $(window).on('scroll', function(){
+    if (content.offset().top + content.height() > $(document).scrollTop() + window.innerHeight) {
+      return;
+    }
+    if (loading || !nextUrl) { return; }
+
+    loading = true
+    $.get(nextUrl, function(data){
+      var partial = $('<div/>').append(data);
+      partial.find('.conversations').children().appendTo(content);
+      nextUrl = partial.find('a[rel=next]').attr('href');
+      if (!nextUrl) { $(window).off('scroll', '**'); }
+      loading = false;
+    });
+  });
+});
